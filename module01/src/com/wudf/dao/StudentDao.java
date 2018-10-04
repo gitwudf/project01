18d19a17ae3cf19c5f86227a76cc8facdbcda857
gitwudf <2321624999@qq.com>
2018-10-04 16:39:35 +0800

    添加接口

--- module01/src/com/wudf/dao/StudentDao.java
+++ module01/src/com/wudf/dao/StudentDao.java
@@ -1,6 +1,12 @@
 package com.wudf.dao;
 
+import com.wudf.entity.Student;
+
 public interface StudentDao {
-
-
+    /**
+     * 添加学生
+     * @param student
+     * @return
+     */
+    Boolean addStudent(Student student);
 }
